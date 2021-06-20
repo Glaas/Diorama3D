8184db248f743682bb28dd08370f1dc5fecfc895
Glaas <s.decuyper314@gmail.com>
2021-06-20 14:26:25 +0200

    GROS MENAGE

--- Art for games Diorama/Assets/UVAdjusting.cs
+++ Art for games Diorama/Assets/UVAdjusting.cs
@@ -3,26 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteAlways]
 public class UVAdjusting : MonoBehaviour
 {
     public Vector2 textOffset = Vector2.one;
     public Vector2 textScale = Vector2.one;
-    public MeshFilter filter;
-    public Mesh mesh;
     public MeshRenderer meshRenderer;
-
-    private void Start()
-    {
-        meshRenderer = GetComponent<MeshRenderer>();
-        filter = GetComponent<MeshFilter>();
-        mesh = filter.mesh;
-        
-    }
+    
+    
 
     void Update()
     {
+        meshRenderer = GetComponent<MeshRenderer>();
         meshRenderer.material.mainTextureOffset = textOffset;
         meshRenderer.material.mainTextureScale = textScale;
+        
     }
 }
