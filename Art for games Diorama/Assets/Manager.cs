28cca31ff077f7a70788ec478aeec3af24872381
Sebastien "Glaas" Decuyper <s.decuyper314@gmail.com>
2021-06-28 14:14:49 +0200

    yee

--- Art for games Diorama/Assets/Manager.cs
+++ Art for games Diorama/Assets/Manager.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject glassDome;
+    public bool isDomeOn = true;
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MoveDome()
     {
-        
+        string msg = isDomeOn ? "Off" : "On ";
+        print($"Dome is now {isDomeOn}");
+        float yval = isDomeOn ? 50 : 0;
+        glassDome.transform.DOLocalMoveY(yval, 2).SetEase(Ease.InOutBack);
+        isDomeOn = isDomeOn != true;
     }
 }
